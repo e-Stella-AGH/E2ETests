4d983b4abee537a692ea58ab4f510ca26e85fe99
Davenury <dawid199960@gmail.com>
2021-11-06 22:07:03 +0100

    first E-Stella test

--- index.js
+++ index.js
@@ -1,14 +1,34 @@
-import { Selector } from 'testcafe';
+import { Selector, Role } from 'testcafe';
 
-fixture `Getting Started`
-    .page `https://devexpress.github.io/testcafe/example/`;
+// fixture `Getting Started`
+//     .page `https://devexpress.github.io/testcafe/example/`;
 
-test("My first test", async t => {
+// test("My first test", async t => {
+//     await t
+//         .typeText('#developer-name', 'Peter Parker')
+//         .click('#submit-button')
+//         .expect(Selector('.result-content').find('h1').innerText).eql("Thank you, Peter Parker!")
+// })
+
+const hrRole = Role('https://e-stella-agh.github.io/MainFrontApp/#/login', async t => {
     await t
-        .typeText('#developer-name', 'Peter Parker')
-        .click('#submit-button')
+        .typeText(Selector('input[name=login]'), "principus@roma.com")
+        .typeText(Selector('input[name=password]'), "a")
+        .click(Selector('button[type=submit]').withText('LOGIN'))
+})
 
-    const articleHeader = await Selector.apply('.result-content').find('h1')
-    let headerText = await articleHeader.innerText
-    console.log(headerText)
+fixture `E-Stella`
+    .page `https://e-stella-agh.github.io/MainFrontApp/#/`;
+
+test("HR log in test", async t => {
+    await t
+        .click(Selector('#loginButton').withText('LOGIN'))
+        .typeText(Selector('input[name=login]'), "principus@roma.com")
+        .typeText(Selector('input[name=password]'), "a")
+        .click(Selector('button[type=submit]').withText('LOGIN'))
+        .expect(Selector('#swal2-title').innerText).eql("Successfully logged in!")
+})
+
+test("Should be able to create offer as HR", async t => {
+    
 })
